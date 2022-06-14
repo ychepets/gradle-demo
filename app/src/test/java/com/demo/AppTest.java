d2f2375d1ccf029a96fc9d94dae838cbae62b67d
Yurii Chepets <ychepets@gmail.com>
2022-06-14 21:58:02 +0300

    add gradle application project Java/TestNG

---
+++ app/src/test/java/com/demo/AppTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class AppTest {
+    @Test public void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
